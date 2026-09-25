1e0e75a0268c2896cf42afc1a3a10f890243a9d8
camila-lazon <camila.lazon@utec.edu.pe>
2026-09-24 23:43:49 -0500

    Vector corregido

--- main.cpp
+++ main.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <string>
-#include "supermario.cpp"
+#include <sstream>
 #include <vector>
+#include "supermario.cpp"
 using namespace std;
 
 int main() {
-    string move;
+    string linea;
     crear_world();
-    vector<string>movimientos;
     bool booleano = false;
     int playerRow = 28;
     int playerCol = 5;
+    vector<string> movimientos;
 
     cout<<"$ init"<<"\n";
     cout<<"$ Welcome to the world of super mario bros xyz"<<"\n";
     draw_world( playerRow, playerCol);
     render();
 
-    while (cin >> move && coins != 3) {
-        movimientos.push_back(move);
+    while (getline(cin, linea) && coins != 3) {
+        movimientos.push_back(linea);
         cout<<"$ init"<<"\n";
-
         for (size_t i = 0; i < movimientos.size(); i++) {
             cout<<"$ "<<movimientos[i]<<"\n";
         }
 
-        move_player(playerRow,playerCol, move, booleano);
-        draw_world( playerRow, playerCol);
-        review(playerRow, playerCol);
-        upcoins(playerRow, playerCol);
-        render();
+        stringstream ss(linea);
+        string move;
+        while (ss >> move) {
+            move_player(playerRow,playerCol, move, booleano);
+            draw_world( playerRow, playerCol);
+            review(playerRow, playerCol);
+            upcoins(playerRow, playerCol);
+            render();
+        }
 
         cout << "$ Coins = " << coins << "\n";
         if (Over(playerRow, playerCol + 12) || booleano) {
